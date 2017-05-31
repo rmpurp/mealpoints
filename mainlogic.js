c115986bee26fbafa617439deee1e89836d8de8c
superiorbocal <waldron.ricky@yahoo.com>
2017-05-31 10:19:19 -0700

    Files combined; behavior when switching tabs is a bit buggy

--- mainlogic.js
+++ mainlogic.js
@@ -8,39 +8,61 @@ var d = {
     "breakStart":"1999 4 22",
     "breakEnd":"1999 4 23",
     "semesterEnd":"1999 4 24"
-}; //dates
+}; //to be replaced
 
-var currentDate;
+var currentDate; //eventually remove this global
 
-function loadMain() {
+function loadMainPage() {
+    var date = new Date("11 23 2017") //eventaully change to new Date()
+    onPageLoad(function() {
+        updateFraction(date, "It's ", "% through the semester!", "Happy Thanksgiving! ");
+        updateMealPlanForcast(date);
+        $(".current_date").html(date.toLocaleString());
+    });
 
 }
 
-function loadSlider() {
-
+function loadSliderPage() {
+    var date = new Date("11 23 2017")
+    onPageLoad(function() {
+        setUpSlider(date);
+    })
 }
 
-function onPageLoad() {
-    $.get('dates.json', undefined, function(data) {
+function onSliderChange() {
+    var date = new Date(parseInt($("#datePicker").val()));
+    $(".current_date").html(date.toLocaleString());
+    updateFraction(date, "This would be ", "% through the semester!", "This is Thanksgiving break! ");
+    updateMealPlanForcast(date);
+}
+
+function setUpSlider(today) {
+    $("#datePicker").attr({
+        "min": d.semesterStart.getTime(),
+        "max": d.semesterEnd.getTime(),
+        "value": today.getTime()
+    });
+    onSliderChange();
+}
+
+function onPageLoad(afterLoad) {
+    var promise = $.get('dates.json', undefined, function(data) {
         d = data;
         for(var key in d) {
             if(d.hasOwnProperty(key)) {
                 d[key] = new Date(d[key]);
             }
         }
-        currentDate = new Date("10 12 2017"); //change to new Date() for production purposes
-        updateFraction(currentDate);
-        updateMealPlanForcast(currentDate);
-        $("#current_date").html(currentDate.toLocaleString());
     }, 'json');
 
+    promise.always(afterLoad);
 }
 
 
 function updateMealPlanForcast(date) {
     var numMealsLeft = 1500 - 1500 * fractionThroughTheYear(date);
     numMealsLeft = Math.round(10 * numMealsLeft) / 10;
-    $("#meal_forecast").html("You should have " + numMealsLeft + " meal points left.");
+    $(".meal_forecast").html("You should have " + numMealsLeft + " meal points left.");
 }
 
 function fractionThroughTheYear(date) {
@@ -64,15 +86,29 @@ function getAdjustedTimeSinceStart(date) {
     }
 }
 
-function updateFraction(date) {
+function updateFraction(date, beforeString, afterString, whenBreakString) {
     date = date || currentDate;
+    if(date < d.semesterStart) {
+        $(".date_string").html("The semester hasn't even started yet!");
+    }
+
+    if(date > d.semesterEnd) {
+        $(".date_string").html("The semester's done!");
+    }
+
+    if(date > d.breakStart && date < d.breakEnd) {
+        beforeString = whenBreakString + beforeString;
+    }
+
     var roundedPercentage = Math.round(1000 * fractionThroughTheYear(date)) / 10;
-    $("#date_string").html("It's " + roundedPercentage + "% through the semester!");
+    $(".date_string").html(beforeString + roundedPercentage + afterString);
 }
 
+/*
 function runThroughDates() {
     for(var i = d.semesterStart.getTime(); i <= d.semesterEnd.getTime(); i += MSEC_PER_DAY) {
         var date = new Date(i);
         console.log(date.toDateString() + " " + fractionThroughTheYear(date));
     }
 }
+*/
